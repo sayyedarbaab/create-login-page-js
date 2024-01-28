7e8ece48abb01bf9a9a5ad9166f5772b6deb04b2
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-01-28 19:41:00 +0530

    add commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let signupBtn=document.getElementById("signupBtn");
+let signinBtn=document.getElementById("signinBtn");
+let nameField=document.getElementById("nameField");
+let title=document.getElementById("title");
+
