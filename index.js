797f3b1d8d26c21e8485e2c74bb7b2df283d365c
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-01-29 17:13:44 +0530

    add commit

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ let signupBtn=document.getElementById("signupBtn");
 let signinBtn=document.getElementById("signinBtn");
 let nameField=document.getElementById("nameField");
 let title=document.getElementById("title");
+let lastnameField=document.getElementById("lastnameField")
 
+
+
+signinBtn.onclick= function(){
+    nameField.style.maxHeight= "0";
+    lastnameField.style.maxHeight="0";
+    title.innerHTML="Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+}
+signupBtn.onclick= function(){
+    nameField.style.maxHeight= "60px";
+    lastnameField.style.maxHeight="60px";   
+    title.innerHTML="Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+}
